14672319cfc689c63bc84b910b408f9b7bbec67f
August Skarsfjord Nyheim <august.nyheim@gmail.com>
2025-01-02 20:25:16 +0100

    Added types

---
+++ service/database/types.go
@@ -0,0 +1,34 @@
+package database
+
+type User struct {
+	UserID        int64   `json:"userId"`
+	Username      string  `json:"username"`
+	PhotoUrl      string  `json:"photoUrl"`
+	Conversations []int64 `json:"conversations"`
+}
+
+type Comment struct {
+	Emoticon string `json:"emoticon"`
+	UserID   string `json:"userId"`
+}
+
+type Message struct {
+	MessageID        int64     `json:"messageId"`
+	UserID           int64     `json:"userId"`
+	Content          string    `json:"content"`
+	Timestamp        string    `json:"timestamp"`
+	Status           string    `json:"status"`
+	IsReply          bool      `json:"isReply"`
+	OriginalMessageID int64    `json:"originalMessageId"`
+	IsForwarded      bool      `json:"isForwarded"`
+	Comments         []Comment `json:"comments"`
+}
+
+type Conversation struct {
+	ConversationID   int64   `json:"conversationId"`
+	Name             string  `json:"name"`
+	ConversationType string  `json:"conversationType"`
+	Users            []int64 `json:"users"`
+	LastMessageID    int64   `json:"lastMessageId"`
+	PhotoUrl         string  `json:"photoUrl"`
+}
